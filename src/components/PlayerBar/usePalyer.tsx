62ea2781879b3af419dab5e8ec2b12666dbf599d
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-10-10 14:53:34 +0300

    play

--- src/components/PlayerBar/usePalyer.tsx
+++ src/components/PlayerBar/usePalyer.tsx
@@ -14,14 +14,16 @@ export const usePlayer = <T extends { src: string }>({
   const [isPlaying, setIsPlaying] = useState(false);
   const [trackDuration, setTrackDuration] = useState(0);
   const [currentTrackDuration, setCurrentTrackDuration] = useState(0);
-  const [currentVolume, setcurrentVolume] = useState(0);
+  const [currentVolume, setCurrentVolume] = useState(0);
   const [isPrevDisabled, setPrevDisabled] = useState(true);
   const [isNextDisabled, setNextDisabled] = useState(true);
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+  const [isFirstPlay, setIsFirstPlay] = useState(false);
 
   useEffect(() => {
     const newAudio = new Audio();
     newAudio.volume = 0.5;
-    setcurrentVolume(newAudio.volume);
+    setCurrentVolume(newAudio.volume);
     setAudio(newAudio);
 
     return () => {
@@ -29,17 +31,51 @@ export const usePlayer = <T extends { src: string }>({
     };
   }, []);
 
+  useEffect(() => {
+    const handleUserGesture = () => {
+      if (window.AudioContext) {
+        const newAudioCtx = new window.AudioContext();
+
+        newAudioCtx.resume().then(() => {
+          console.log('Audio context resumed');
+        });
+        setAudioContext(newAudioCtx);
+
+        document.removeEventListener('click', handleUserGesture);
+        document.removeEventListener('touchend', handleUserGesture);
+      }
+    };
+
+    document.addEventListener('click', handleUserGesture);
+    document.addEventListener('touchend', handleUserGesture);
+
+    return () => {
+      document.removeEventListener('click', handleUserGesture);
+      document.removeEventListener('touchend', handleUserGesture);
+    };
+  }, []);
+
   const play = useCallback(() => {
-    if (audio) {
+    console.log('play');
+
+    if (audio && !isPlaying) {
+      if (!isFirstPlay) {
+        setIsFirstPlay(true);
+      }
       if (audio.readyState === HTMLMediaElement.HAVE_NOTHING) {
         audio.src = queue[currentTrackIndex].src;
         audio.load();
       }
-      audio.play();
+      audio.play().catch((error) => {
+        console.error('Ошибка воспроизведения:', error);
+        alert('Ошибка воспроизведения: ' + error.message);
+      });
     }
-  }, [audio, currentTrackIndex]);
+  }, [audio, currentTrackIndex, queue, isFirstPlay, isPlaying]);
 
   const pause = useCallback(() => {
+    console.log('pause');
+
     if (audio) {
       audio.pause();
       setIsPlaying(!audio.paused);
@@ -56,18 +92,27 @@ export const usePlayer = <T extends { src: string }>({
             const callback = () => {
               audio.removeEventListener('loadstart', callback);
               audio.removeEventListener('abort', callback);
-              resolve();
+              try {
+                resolve();
+                console.log('loadAndPlay resolve');
+              } catch (error) {
+                console.log(error.message);
+              }
+
+              console.log('loadAndPlay callback');
             };
             audio.addEventListener('loadstart', callback);
             audio.addEventListener('abort', callback);
           });
 
-          await audio.load();
+          audio.load();
           await awaiter;
           await audio.play();
+          console.log('loadAndPlay');
         } catch (error) {
           if (error instanceof MediaError && error.code !== MediaError.MEDIA_ERR_ABORTED) {
             console.error('Ошибка:', error);
+            alert('Ошибка:');
           }
         }
       }
@@ -94,14 +139,15 @@ export const usePlayer = <T extends { src: string }>({
         return audio?.pause();
       }
     }
-
+    setCurrentTrackDuration(0);
     setCurrentTrackIndex(newIndex);
     await loadAndPlay(queue[newIndex].src);
 
-    if (audio && audio.paused) {
-      audio.play();
+    if (audioContext && audioContext.state !== 'running') {
+      await audioContext.resume();
+      console.log('Audio context resumed after track switch');
     }
-  }, [currentTrackIndex, queue, repeat, loadAndPlay, audio]);
+  }, [currentTrackIndex, queue, repeat, loadAndPlay, audio, audioContext]);
 
   const prev = useCallback(async () => {
     let newIndex = currentTrackIndex - 1;
@@ -131,7 +177,7 @@ export const usePlayer = <T extends { src: string }>({
     if (audio) {
       const volumeValue = Number(volume);
       audio.volume = volumeValue;
-      setcurrentVolume(volumeValue);
+      setCurrentVolume(volumeValue);
     }
   };
 
@@ -148,6 +194,8 @@ export const usePlayer = <T extends { src: string }>({
     };
 
     const handleEnd = () => {
+      console.log('handleEnd');
+
       if (repeat === 'one') {
         audio.currentTime = 0;
         audio.play();
@@ -156,9 +204,12 @@ export const usePlayer = <T extends { src: string }>({
       }
     };
 
-    const handlePlayStop = async () => {
-      await setIsPlaying(!audio.paused);
+    const handlePlayStop = () => {
+      console.log('handlePlayStop');
+
+      setIsPlaying(!audio.paused);
     };
+
     audio.addEventListener('play', handlePlayStop);
     audio.addEventListener('pause', handlePlayStop);
     audio.addEventListener('loadedmetadata', handleLoadedMetadata);
@@ -185,6 +236,7 @@ export const usePlayer = <T extends { src: string }>({
 
   return {
     isPlaying,
+    audioContext,
     pause,
     play,
     next,
