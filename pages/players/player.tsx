1d510c30c3c2253f746e66d9a9ed4f43f38254ad
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-10-12 00:26:05 +0300

    ssr

--- pages/players/player.tsx
+++ pages/players/player.tsx
@@ -1,15 +1,23 @@
 import { trackList } from '../../public/TrackLists/trackListBarbershop';
 import PlayerBar from '../../src/components/PlayerBar/PlayerBar';
 
-function Player() {
+export async function getServerSideProps() {
+  return {
+    props: {
+      trackList,
+    },
+  };
+}
+
+function Player({ trackList }) {
   return (
     <>
       <meta
         name="description"
         content="Автор: Звук Бизнес, Категория: Музыкальный плейлист, Цена: Бесплатно, Длительность: 120 минут, Описание: Наслаждайтесь специально подобранным плейлистом для барбершопов и мужских парикмахерских, включающим расслабляющие и стильные треки для улучшения вашего опыта ухода."
-      ></meta>
+      />
       <title>Плейлист для барбершопа от Звук Бизнес</title>
-      <PlayerBar trackList={trackList} />;
+      <PlayerBar trackList={trackList} />
     </>
   );
 }
