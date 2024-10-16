58e277ad8a5363c9e3e113768c187d5faeea5494
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-10-16 16:06:17 +0300

    equalaizer css changes

--- src/components/Equalizer/Equalizer.tsx
+++ src/components/Equalizer/Equalizer.tsx
@@ -4,7 +4,7 @@ import styles from './Equalizer.module.scss';
 const Equalizer: React.FC = () => {
   return (
     <div className={styles.equalizer_wrapper}>
-      <span className={`${styles.playing__bar} ${styles.playing__bar1}`}></span>
+      {/* <span className={`${styles.playing__bar} ${styles.playing__bar1}`}></span> */}
       <span className={`${styles.playing__bar} ${styles.playing__bar3}`}></span>
       <span className={`${styles.playing__bar} ${styles.playing__bar2}`}></span>
       <span className={`${styles.playing__bar} ${styles.playing__bar4}`}></span>
