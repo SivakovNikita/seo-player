9f611d64f5a9dcca1414fe1a5c5d686bd9779fec
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-10-22 17:54:30 +0300

    Chill Pop Playlist

---
+++ public/TrackLists/trackListChillPop.ts
@@ -0,0 +1,382 @@
+export const trackListChillPop = [
+  {
+    title: 'Мелодия утреннего света',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_1.mp3',
+    artist: 'Звук Бизнес',
+    duration: '04:28',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Тихие шаги',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_2.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:16',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Ветер перемен',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_3.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:52',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Поток времени',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_4.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:43',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Сон в облаках',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_5.mp3',
+    artist: 'Звук Бизнес',
+    duration: '06:29',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Тёплый бриз',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_6.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:10',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Светлый день',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_7.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:20',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Мгновение покоя',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_8.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:37',
+    img: [
+      {
+        src: '/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Пробуждение природы',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_9.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:11',
+    img: [
+      {
+        src: '/api/images?file=/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+  {
+    title: 'Спокойствие на душе',
+    src: '/api/audio?file=Tracks/chill-pop/chill-pop_10.mp3',
+    artist: 'Звук Бизнес',
+    duration: '03:06',
+    img: [
+      {
+        src: '/api/images?file=/images/trackImage/trackCover.jpg',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_128x128.png',
+        sizes: '128x128',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/api/images?file=/images/trackImage/trackCover_512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+];
