84b2d02bad159fa9bfd547831c267863b0bce67d
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-11-14 10:31:30 +0300

    player bar v1.6

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,23 +1,14 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import TimerBar from '../../components/TimerBar/TimerBar';
-import ProgressBar from '../../components/ProgressBar/ProgressBar';
-import SideTimersProgressBar from '../../components/SideTimersProgressBar/SideTimersProgressBar';
 
 function MyApp() {
   const router = useRouter();
 
-  // useEffect(() => {
-  //   router.push('https://zvuk-b2b.com/?utm_source=seo-player&utm_medium=redirect');
-  // }, [router]);
-  const currentTime = 7.235716;
-  const duration = 18.076735;
-  // return null;
-  return (
-    <div style={{ margin: '50px' }}>
-      <SideTimersProgressBar currentTrackDuration={currentTime} duration={duration} />
-    </div>
-  );
+  useEffect(() => {
+    router.push('https://zvuk-b2b.com/?utm_source=seo-player&utm_medium=redirect');
+  }, [router]);
+
+  return null;
 }
 
 export default MyApp;
