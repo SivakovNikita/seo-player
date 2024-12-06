9d142680f78f05b407a9a4c9dcf598da31828793
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-12-06 21:35:54 +0300

    Clerk auth added and styles updates 1.8

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,14 +1,18 @@
 import React, { Suspense } from 'react';
 import Layout from '../../src/components/AdminPanel/Layout/Layout';
 import WelcomePage from '../../src/components/AdminPanel/Pages/WelcomePage/WelcomePage';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const AdminPanelPage = () => {
+  const PUBLISHABLE_KEY = 'pk_test_ZnVsbC1qb2V5LTgxLmNsZXJrLmFjY291bnRzLmRldiQ';
   return (
-    <Layout>
-      <Suspense fallback={<div>Loading...</div>}>
-        <WelcomePage />
-      </Suspense>
-    </Layout>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Layout>
+        <Suspense fallback={<div>Loading...</div>}>
+          <WelcomePage />
+        </Suspense>
+      </Layout>
+    </ClerkProvider>
   );
 };
 
