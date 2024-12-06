6c92ba71708ac27ae8c607ea9494832d9cc4009f
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2024-12-06 21:04:32 +0300

    Clerk auth added and styles updates 1.7

--- src/components/AdminPanel/Layout/Layout.tsx
+++ src/components/AdminPanel/Layout/Layout.tsx
@@ -1,14 +1,12 @@
 import Header from '../Header/Header';
-import { ClerkProvider, SignedIn } from '@clerk/nextjs';
+import { SignedIn } from '@clerk/nextjs';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const PUBLISHABLE_KEY = 'pk_test_ZnVsbC1qb2V5LTgxLmNsZXJrLmFjY291bnRzLmRldiQ';
-
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <>
       <Header />
       <SignedIn>{children}</SignedIn>
-    </ClerkProvider>
+    </>
   );
 };
 
