3ccf5d7f151995cdaf79de37e3ca91b4192042d3
Nikita Sivakov <116516988+SivakovNikita@users.noreply.github.com>
2025-02-28 11:25:02 +0300

    embed modal popup styles

--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -18,7 +18,14 @@ const Modal = ({ children, onClose }: ModalProps) => {
   return (
     <div className={styles.pop_up_overlay} onClick={handleOverlayClick}>
       <div className={styles.pop_up} onClick={handleModalClick}>
-        <Image className={styles.logo} src="/images/zvuk business logo.png" width={112} height={30} alt="Звук Бизнес" />
+        <Image
+          className={styles.logo}
+          src="/images/zvuk business logo.png"
+          width={112}
+          height={30}
+          alt="Звук Бизнес"
+          priority={true}
+        />
         <div className={styles.children}>{children}</div>
         <a href="https://zvuk-b2b.com/?utm_source=player&utm_medium=embed&utm_content=about" target="_blank">
           <button onClick={handleOverlayClick} className={styles.button}>
